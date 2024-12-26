5350be9b518830968806398572fe0ca592463fbf
NazarenoLorenzatti <nl.loragro@gmail.com>
2024-12-26 12:42:27 -0300

    Configuraciones

--- src/main/java/ar/com/ultrafibra/intranet/config/SecurityConfig.java
+++ src/main/java/ar/com/ultrafibra/intranet/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(request -> request
-                .requestMatchers(****************).permitAll())
+                .requestMatchers("/intranet/restringed/test/**", "/intranet/restringed/login/**").permitAll())
                 .authorizeHttpRequests((authorize) -> authorize
                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2
