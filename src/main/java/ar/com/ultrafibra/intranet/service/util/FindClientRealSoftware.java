3d3f4c7f5d9a31f43a8c380a1388e461bf526dbf
NazarenoLorenzatti <nl.loragro@gmail.com>
2025-01-29 14:17:52 -0300

    Update 29-01-2025

--- src/main/java/ar/com/ultrafibra/intranet/service/util/FindClientRealSoftware.java
+++ src/main/java/ar/com/ultrafibra/intranet/service/util/FindClientRealSoftware.java
@@ -1,56 +1,29 @@
 package ar.com.ultrafibra.intranet.service.util;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.*;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FindClientRealSoftware extends CreatePassword{
-    
+public class FindClientRealSoftware extends CreatePassword {
     public HttpResponse<JsonNode> getClientRealSoft(String number) {
         HttpResponse<JsonNode> documentTypeMapResponse = getListDocumentType();
         if (documentTypeMapResponse != null) {
-            if (documentTypeMapResponse.getStatus() == 200) {
-                if (documentTypeMapResponse.getBody().getObject().has("tabla")) {
-                    Map<String, String> documentTypeMap = new HashMap();
-                    Gson gson = new Gson();
-                    JsonObject jsonObject = gson.fromJson(documentTypeMapResponse.getBody().toString(), JsonObject.class);
-                    JsonElement clientJson = jsonObject.get("tabla");
-                    Type mapType = new TypeToken<Map<String, String>>() {
-                    }.getType();
-                    documentTypeMap = gson.fromJson(clientJson, mapType);
+            Map<String, String> documentTypeMap = getDocumentTypeMap(documentTypeMapResponse);
+            for (Map.Entry<String, String> entry : documentTypeMap.entrySet()) {
+                String body = String.format("""
+                        {
+                            "action": "cliente",
+                            "tipo_documento": "%s",
+                            "nro_documento": "%s"
+                        } """, entry.getKey(), number);
 
-                    for (Map.Entry<String, String> entry : documentTypeMap.entrySet()) {
-                        try {
-                            HttpResponse<JsonNode> response = Unirest.post("https://api.gestionreal.com.ar/")
-                                    .header("Content-Type", "application/json")
-                                    .basicAuth(this.username, createPassword())
-                                    .body("{ \r\n \"action\": \"cliente\", \r\n\"tipo_documento\":" + entry.getKey() + ", \r\n \"nro_documento\": " + number + " \r\n} \r\n")
-                                    .asJson();
-
-                            Gson gsonResp = new Gson();
-                            JsonObject jsonObjectResp = gsonResp.fromJson(response.getBody().toString(), JsonObject.class);
-                            String errorValue = jsonObjectResp.get("error").getAsString();
-
-                            if (errorValue.equals("0")) {
-                                return response;
-                            }
-                        } catch (UnirestException ex) {
-//                            Logger.getLogger(ClientServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-                            return null;
-                        }
-                    }
-                }
+                return this.getResponse(body);
             }
         }
         return null;
@@ -58,24 +31,44 @@ public class FindClientRealSoftware extends CreatePassword{
 
     // Obtengo la tabla de tipos de Documentos, Para soicitar el cliente a la Api de GR ya que necesita un body con 2 parametros, Tipo y Numero de documento.
     private HttpResponse<JsonNode> getListDocumentType() {
-        try {
-            HttpResponse<JsonNode> response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, this.createPassword())
-                    .body("{\r\n \"action\": \"tabref\",\r\n \"tabla\": \"tipodocumento\"\r\n}\r\n")
-                    .asJson();
+        String body = String.format("""
+            {
+                "action": "tabref",
+                "tabla": "tipodocumento"
+            }
+            """);
+        return this.getResponse(body);
+    }
 
+    private Map<String, String> getDocumentTypeMap(HttpResponse<JsonNode> documentTypeMapResponse) {
+        if (documentTypeMapResponse.getBody().getObject().has("tabla")) {
+            return null;
+        }
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(documentTypeMapResponse.getBody().toString(), JsonObject.class);
+        JsonElement clientJson = jsonObject.get("tabla");
+        Type mapType = new TypeToken<Map<String, String>>() {
+        }.getType();
+        Map<String, String> documentTypeMap = gson.fromJson(clientJson, mapType);
+        return documentTypeMap;
+    }
+
+    private HttpResponse<JsonNode> getResponse(String body) {
+        HttpResponse<JsonNode> response = null;
+        try {
+            response = Unirest.post("https://api.gestionreal.com.ar/")
+                    .header("Content-Type", "application/json")
+                    .basicAuth(this.username, createPassword())
+                    .body(body)
+                    .asJson();
             Gson gson = new Gson();
             JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
             String errorValue = jsonObject.get("error").getAsString();
-
-            if (errorValue.equals("0")) {
+            if (response.getStatus() == 200 && errorValue.equals("0")) {
                 return response;
             }
-
         } catch (UnirestException ex) {
-//            Logger.getLogger(ClientServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
--- src/main/java/ar/com/ultrafibra/intranet/service/util/RequestsApiGestionReal.java
+++ src/main/java/ar/com/ultrafibra/intranet/service/util/RequestsApiGestionReal.java
@@ -8,7 +8,6 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,59 +25,6 @@ public class RequestsApiGestionReal extends CreatePassword {
         return this.getJsonObjectResponse(this.getResponse(body));
     }
 
-    /* public JsonObject getResponseCommon(long offset, String action, String date) {
-        String body = String.format("""
-            {
-                "action": "%s",
-                "cantidad": 100,
-                "fecha_desde": "%s",
-                "offset": %d
-            }
-            """, action, date, offset);
-
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{\r\n \"action\": \"" + action + "\", \r\n \"cantidad\": 100,\r\n \"fecha_desde\": \"" + date + "\",\r\n \"offset\": " + offset + "\r\n}\r\n")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            String errorValue = jsonObject.get("error").getAsString();
-            if (errorValue.equals("0")) {
-                return jsonObject;
-            }
-        }
-        return null;
-    }
-
-    public JsonObject getResponseInvoice(long offset, String ofDate) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{\r\n \"action\": \"cmpoficiales\", \r\n \"cantidad\": 100,\r\n \"fecha_desde\": \"" + ofDate + "\",\r\n \"offset\": " + offset + "\r\n  }\r\n")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            String errorValue = jsonObject.get("error").getAsString();
-            if (errorValue.equals("0")) {
-                return jsonObject;
-            }
-        }
-        return null;
-    }
-     */
     public JsonObject getResponseClient(String date, String type_date, long offset) {
         String body = String.format("""
             {
@@ -91,29 +37,6 @@ public class RequestsApiGestionReal extends CreatePassword {
         return this.getJsonObjectResponse(this.getResponse(body));
     }
 
-    /*
-    public JsonObject getResponseClient(String date, String type_date, long offset) {
-        
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{ \r\n \"action\": \"clientes_consulta\",\r\n \"fecha_tipo\": \"" + type_date + "\",\r\n \"fecha_desde\": \"" + date + "\",\r\n \"offset\": " + offset + "\r\n} \r\n")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            String errorValue = jsonObject.get("error").getAsString();
-            if (errorValue.equals("0")) {
-                return jsonObject;
-            }
-        }
-        return null;
-    }*/
     public JsonObject getResponseContract(long idClient) {
         String body = String.format("""
             {
@@ -125,28 +48,6 @@ public class RequestsApiGestionReal extends CreatePassword {
         return this.getJsonObjectResponse(this.getResponse(body));
     }
 
-    /*
-    public JsonObject getResponseContract(long idClient) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{ \r\n    \"action\": \"contrato\",\r\n    \"incluye_bajas\":\"S\",\r\n    \"cli_id\": " + idClient + "\r\n}")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            String errorValue = jsonObject.get("error").getAsString();
-            if (errorValue.equals("0")) {
-                return jsonObject;
-            }
-        }
-        return null;
-    }*/
     public JsonObject getResponseUpdateContract(String date, String type_date) {
         String body = String.format("""
             {
@@ -157,31 +58,6 @@ public class RequestsApiGestionReal extends CreatePassword {
             """, type_date, date);
         return this.getJsonObjectResponse(this.getResponse(body));
     }
-
-    /*public JsonObject getResponseUpdateContract(String date, String type_date) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{ \r\n \"action\": \"contratos\",\r\n \"fecha_tipo\": \"" + type_date + "\",\r\n \"fecha_desde\": \"" + date + "\"\r\n} \r\n")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            String errorValue = jsonObject.get("error").getAsString();
-            if (errorValue.equals("0")) {
-                return jsonObject;
-            }
-        }
-        return null;
-    }*/
-    
-    
-    
     
     public JsonObject getResponseTicket(long offset, String typeDate, String date) {
         String body = String.format("""
@@ -195,35 +71,6 @@ public class RequestsApiGestionReal extends CreatePassword {
 
         return this.getJsonObjectResponseAlternative(this.getResponse(body));
     }
-
-
-    /*public JsonObject getResponseTicket(long offset, String typeDate, String date) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{\r\n\"action\": \"casos\", \r\n\"fecha_tipo\": \"" + typeDate + "\",\r\n\"fecha_desde\": \"" + date + "\",\r\n\"offset\":" + offset + "\r\n}")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-
-            if (response.getStatus() == 200) {
-                Gson gson = new Gson();
-                JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-                return jsonObject;
-            } else {
-                return null;
-            }
-        }
-        return null;
-
-    }*/
-    
-    
-     
      
     public JsonObject getResponseOrderService(String date, String type_date) {
         String body = String.format("""
@@ -235,72 +82,7 @@ public class RequestsApiGestionReal extends CreatePassword {
             """, type_date, date);
         return this.getJsonObjectResponseAlternative(this.getResponse(body));
     }
-    
-    /*
-    public JsonObject getResponseOrderService(String date, String type_date) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{ \r\n \"action\": \"ordenesdeservicio\",\r\n \"fecha_tipo\": \"" + type_date + "\",\r\n \"fecha_desde\": \"" + date + "\"\r\n} \r\n")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-            if (!jsonObject.isEmpty()) {
-                return jsonObject;
-            }
-
-        }
-
-        return null;
-    }*/
-
-    
-    /*
-    
-    METODO NO EN USO!
-    
-    public JsonObject getResponseTicket2(long cliId) {
-       String body = String.format("""
-            {
-                "action": "casos",
-                "caso_id": %d
-            }
-            """, cliId);
-        return this.getJsonObjectResponseAlternative(this.getResponse(body));
-    }*/
-    
-    /*
-    public JsonObject getResponseTicket(long cliId) {
-        HttpResponse<JsonNode> response = null;
-        try {
-            response = Unirest.post("https://api.gestionreal.com.ar/")
-                    .header("Content-Type", "application/json")
-                    .basicAuth(this.username, createPassword())
-                    .body("{\r\n \"action\": \"casos\", \r\n\"caso_id\": " + cliId + "\r\n}")
-                    .asJson();
-        } catch (UnirestException ex) {
-            Logger.getLogger(RequestsApiGestionReal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (response != null) {
-            if (response.getStatus() == 200) {
-                Gson gson = new Gson();
-                JsonObject jsonObject = gson.fromJson(response.getBody().toString(), JsonObject.class);
-                return jsonObject;
-            } else {
-                return null;
-            }
-        }
-        return null;
-    }*/
-    
-    
-
+  
     // Unificacion de Metodos //
     private HttpResponse<JsonNode> getResponse(String body) {
         HttpResponse<JsonNode> response = null;
